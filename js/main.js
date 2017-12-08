f99c805f93a3f85462f52a5c1f53ec7ccd93081a
dannym.8307 <dannym.8307@gmail.com>
2017-12-07 19:11:55 -0500

    dec7

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$('.btn').on("click", function(){
-  console.log($(this).attr("id"));
+$( document ).ready(function() {
+    console.log( "ready!" );
+  $('#btn').on("click", function(){
+    $('.movie-poster-one-bg').addClass('grow-left');
+    $('.movie-poster-two-bg').addClass('grow-right');
+    $('.vs-btn').addClass('fade-in');
+  console.log("button has been clicked")
+    
+})
+               
 });
